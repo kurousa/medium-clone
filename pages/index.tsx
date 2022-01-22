bcde84f02aba73dad070940febcc895b308c223b
kurousa <r.takahashi.1026@gmail.com>
2022-01-22 20:58:25 +0900

    add Sanity, and get as server side props.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import {sanityClient} from "../lib/sanity";
+import { Post } from '../typings';
 
-export default function Home() {
+interface Props {
+  posts: [Post];
+}
+
+export default function Home({ posts }: Props ) {
+
   return (
     <div className='max-w-7xl mx-auto'>
       <Head>
@@ -41,7 +48,30 @@ export default function Home() {
 
       </div>
 
-      
+      {/* Posts */}
+
     </div>
   )
 }
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author-> {
+    name,
+    image
+    },
+    description,
+    mainImage,
+    slug
+  }`;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props:{
+      posts,
+    }
+  }
+};
