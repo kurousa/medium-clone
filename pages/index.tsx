7bb738c39a6a732a4430c8e0718fee4bc98378e9
kurousa <r.takahashi.1026@gmail.com>
2022-01-22 21:00:20 +0900

    format with prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,15 @@
 import Head from 'next/head'
 import Header from '../components/Header'
-import {sanityClient} from "../lib/sanity";
-import { Post } from '../typings';
+import { sanityClient } from '../lib/sanity'
+import { Post } from '../typings'
 
 interface Props {
-  posts: [Post];
+  posts: [Post]
 }
 
-export default function Home({ posts }: Props ) {
-
+export default function Home({ posts }: Props) {
   return (
-    <div className='max-w-7xl mx-auto'>
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Medium</title>
         <link rel="icon" href="/favicon.ico" />
@@ -18,16 +17,17 @@ export default function Home({ posts }: Props ) {
 
       <Header />
 
-      <div className='flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0'>
-        <div className='px-10 space-y-5'>
-          <h1 className='text-6xl max-w-xl font-serif'>
-            <span className='underline decoration-black decoration-4'>
+      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+        <div className="px-10 space-y-5">
+          <h1 className="text-6xl max-w-xl font-serif">
+            <span className="underline decoration-black decoration-4">
               Medium
-            </span>{" "}
+            </span>{' '}
             is a place to write, read and connect
           </h1>
           <h2>
-            It's easy and free to post your thinking on any topic and connect with millions of readers.
+            It's easy and free to post your thinking on any topic and connect
+            with millions of readers.
           </h2>
           <div>
             <h3 className="text-black bg-white px-4 py-1 border-2 border-slate-600  rounded-full w-fit cursor-pointer">
@@ -37,19 +37,17 @@ export default function Home({ posts }: Props ) {
         </div>
 
         <div>
-          <img 
-            className='hidden md:inline-flex lg:h-full px-10'
-            src="/img/medium-logo-trademark.png" 
+          <img
+            className="hidden md:inline-flex lg:h-full px-10"
+            src="/img/medium-logo-trademark.png"
             alt="medium-logo-trademark"
             width={500}
             height={500}
           />
         </div>
-
       </div>
 
       {/* Posts */}
-
     </div>
   )
 }
@@ -65,13 +63,13 @@ export const getServerSideProps = async () => {
     description,
     mainImage,
     slug
-  }`;
+  }`
 
-  const posts = await sanityClient.fetch(query);
+  const posts = await sanityClient.fetch(query)
 
   return {
-    props:{
+    props: {
       posts,
-    }
+    },
   }
-};
+}
