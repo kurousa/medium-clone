da43d31d38ecfc7ab10174322aadcb0c7f414290
kurousa <r.takahashi.1026@gmail.com>
2022-01-22 10:42:28 +0900

    add Header components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div>
       <Head>
         <title>Medium</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head>     
+      </Head>
+
+      <Header />
+      
     </div>
   )
 }
